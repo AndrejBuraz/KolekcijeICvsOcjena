c0f4aa56c8fc7940cf71670d33da4126ade84885
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-03-15 09:03:03 +0100

    Add files via upload

---
+++ OcjenskaVjezbaKolekcije/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace OcjenskaVjezbaKolekcije
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnIspisi = new System.Windows.Forms.Button();
+            this.btnSprei = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(27, 55);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prezime";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(27, 106);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Godina rođenja";
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(27, 155);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(100, 20);
+            this.txtBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Spol";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "M",
+            "Ž"});
+            this.comboBox1.Location = new System.Drawing.Point(27, 207);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 258);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(900, 211);
+            this.textBox1.TabIndex = 9;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnUnesi.Location = new System.Drawing.Point(171, 55);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(176, 173);
+            this.btnUnesi.TabIndex = 10;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = false;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSpremi.Location = new System.Drawing.Point(362, 55);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(176, 173);
+            this.btnSpremi.TabIndex = 11;
+            this.btnSpremi.Text = "Obradi";
+            this.btnSpremi.UseVisualStyleBackColor = false;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // btnIspisi
+            // 
+            this.btnIspisi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnIspisi.Location = new System.Drawing.Point(555, 55);
+            this.btnIspisi.Name = "btnIspisi";
+            this.btnIspisi.Size = new System.Drawing.Size(176, 173);
+            this.btnIspisi.TabIndex = 12;
+            this.btnIspisi.Text = "Ispiši";
+            this.btnIspisi.UseVisualStyleBackColor = false;
+            this.btnIspisi.Click += new System.EventHandler(this.btnIspisi_Click);
+            // 
+            // btnSprei
+            // 
+            this.btnSprei.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSprei.Location = new System.Drawing.Point(751, 59);
+            this.btnSprei.Name = "btnSprei";
+            this.btnSprei.Size = new System.Drawing.Size(176, 173);
+            this.btnSprei.TabIndex = 13;
+            this.btnSprei.Text = "Spremi";
+            this.btnSprei.UseVisualStyleBackColor = false;
+            this.btnSprei.Click += new System.EventHandler(this.btnSprei_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(948, 481);
+            this.Controls.Add(this.btnSprei);
+            this.Controls.Add(this.btnIspisi);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnIspisi;
+        private System.Windows.Forms.Button btnSprei;
+    }
+}
+
---
+++ OcjenskaVjezbaKolekcije/Form1.cs
@@ -0,0 +1,119 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OcjenskaVjezbaKolekcije
+{
+    public partial class Form1 : Form
+    {
+        List<Osoba> listaOsoba = new List<Osoba>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Osoba osoba = new Osoba(txtBox1.Text, txtBox2.Text, Convert.ToInt32(txtBox3.Text), comboBox1.Text);
+                listaOsoba.Add(osoba);
+                txtBox1.Clear();
+                txtBox2.Clear();
+                txtBox3.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnIspisi_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Text = ("Ime\t\t" + "Prezime\t\t" + "Godina rodenja\t\t" + "Spol\t\t" + "Dodatak");
+            textBox1.AppendText(Environment.NewLine);
+            foreach (Osoba osoba in listaOsoba)
+            {
+                textBox1.AppendText(osoba.ToString());
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            foreach (Osoba osoba in listaOsoba)
+            {
+                if (osoba.Spol == "M")
+                {
+                    osoba.Dodatak = "Ima brkove";
+                }
+                else
+                {
+                    osoba.Dodatak = "Nema brkove";
+                }
+            }
+        }
+
+        private void btnSprei_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    
+                    myStream.Close();
+                }
+            }
+            using (var stream = File.Open("osobeFile.csv", FileMode.Append))
+            using (var writer = new StreamWriter(stream))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(listaOsoba);
+            }
+        }
+    }
+
+    class Osoba
+    {
+        string ime, prezime;
+        int godRod;
+        string spol, dodatak;
+
+        public Osoba(string ime, string prezime, int godRod, string spol)
+        {
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.GodRod = godRod;
+            this.Spol = spol;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public int GodRod { get => godRod; set => godRod = value; }
+        public string Spol { get => spol; set => spol = value; }
+        public string Dodatak { get => dodatak; set => dodatak = value; }
+
+        public override string ToString()
+        {
+            string ispis = (this.ime + "\t\t" + this.prezime + "\t\t" + this.godRod + "\t\t\t" + this.spol + "\t\t" + this.dodatak);
+            return ispis;
+        }
+    }
+}
